9468f339a0a56675c154174d781d7b327c8691c8
Arjeet Anand <72692370+arjeetanand@users.noreply.github.com>
2026-03-05 09:51:14 +0530

    Time: 2 ms (22.27%) | Memory: 9.3 MB (76.20%) - LeetSync

---
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int n = s.length();
+        int startWith0 = 0;
+        int startWith1 = 0;
+
+        for (int i = 0; i < n; i++) {
+            // Expected character if pattern starts with '0'
+            char expected0 = (i % 2 == 0) ? '0' : '1';
+
+            // Expected character if pattern starts with '1'
+            char expected1 = (i % 2 == 0) ? '1' : '0';
+
+            if (s[i] != expected0) startWith0++;
+            if (s[i] != expected1) startWith1++;
+        }
+
+        return min(startWith0, startWith1);
+    }
+};
