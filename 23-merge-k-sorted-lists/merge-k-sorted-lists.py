ffbe8149218bf7081412e7058e1170c9f0ef962c
Arjeet Anand <72692370+arjeetanand@users.noreply.github.com>
2026-07-07 00:34:33 +0530

    Time: 16 ms (20.85%) | Memory: 22.6 MB (80.72%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -1,22 +1,32 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        minheap = []
-        for node in lists:
-            while node:
-                minheap.append(node.val)
-                node=node.next
-        heapq.heapify(minheap)
+    def mergeKLists(self, lists):
+        if not lists:
+            return None
 
-        dummy=  start= ListNode(None)
-        while minheap:
-            start.next = ListNode(heapq.heappop(minheap))
-            start= start.next
-        
+        while len(lists) > 1:
+            merged = []
+
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if i + 1 < len(lists) else None
+                merged.append(self.mergeTwoLists(l1, l2))
+
+            lists = merged
+
+        return lists[0]
+
+    def mergeTwoLists(self, list1, list2):
+        dummy = ListNode(0)
+        tail = dummy
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+
+        tail.next = list1 if list1 else list2
         return dummy.next
-
-
